3c970f2e5d7866ab65bf417c995eb29781503ee1
taru <taru253@gmail.com>
2024-10-07 15:17:15 +0530

    added new comment for enumerate function

--- Problems/1. Two Sums/1TwoSums.py
+++ Problems/1. Two Sums/1TwoSums.py
@@ -1,7 +1,7 @@
 class Solution(object):
     def twoSum(self, nums, target):
         num_dict = {}  
-        for i, num in enumerate(nums):
+        for i, num in enumerate(nums): # enumerate: to iterate through a sequence and keep track of the index of each element
             diff = target - num 
             if diff in num_dict:
                 return [num_dict[diff], i] 
