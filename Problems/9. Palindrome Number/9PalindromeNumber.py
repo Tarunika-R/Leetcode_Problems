04da2e5ed9c0bc83426338895b22b3c1e2e7a5a3
Tarunika-R <taru253@gmail.com>
2025-06-11 17:30:45 +0530

    `Removed multiple LeetCode problem solutions from the repository.`

--- Problems/9. Palindrome Number/9PalindromeNumber.py
+++
@@ -1,8 +0,0 @@
-class Solution(object):
-    def isPalindrome(self, x):
-        """
-        :type x: int
-        :rtype: bool
-        """
-        self = str(x) == str(x)[::-1]
-        return self
