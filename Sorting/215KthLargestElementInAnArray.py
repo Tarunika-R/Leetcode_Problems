5f4c30fc76d4596e94f188f6baca8b5fc17ed0c6
Tarunika-R <taru253@gmail.com>
2025-07-20 23:18:34 +0530

    sorting

---
+++ Sorting/215KthLargestElementInAnArray.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def findKthLargest(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        nums.sort()
+        for i in range(len(nums)):
+            return nums[-k]
