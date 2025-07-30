b1d20a23f66f67db69d6dabd341615cadbd64b61
Tarunika R <taru253@gmail.com>
2025-07-30 22:31:46 +0530

    `Refactored twoSum method: added alternative implementation using nested loops`

--- Array/1. Two Sums/1TwoSums.py
+++ Array/1. Two Sums/1TwoSums.py
@@ -1,8 +1,14 @@
 class Solution(object):
     def twoSum(self, nums, target):
-        num_dict = {}  
-        for i, num in enumerate(nums): # enumerate: to iterate through a sequence and keep track of the index of each element
-            diff = target - num 
-            if diff in num_dict:
-                return [num_dict[diff], i] 
-            num_dict[num] = i        
+        # num_dict = {}  
+        # for i, num in enumerate(nums): # enumerate: to iterate through a sequence and keep track of the index of each element
+        #     diff = target - num 
+        #     if diff in num_dict:
+        #         return [num_dict[diff], i] 
+        #     num_dict[num] = i        
+
+        for i in range(0, len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+        return []
