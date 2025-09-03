d917a1bf658d0a785d8d75f9a9d062db3e476c58
Tarunika R <127477721+Tarunika-R@users.noreply.github.com>
2025-09-03 10:27:14 +0530

    Time: 445 ms (15.40%) | Memory: 0B (100.00%) - LeetSync

--- 182-duplicate-emails/duplicate-emails.sql
+++ 182-duplicate-emails/duplicate-emails.sql
@@ -1,5 +1,6 @@
 # Write your MySQL query statement below
 SELECT DISTINCT A.Email 
-FROM Person A, Person B
+FROM Person A JOIN Person B
+ON A.email = B.email
 WHERE A.id <> B.id
-AND A.email = B.email 
+  
