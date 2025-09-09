8db4008902035f2c4082bf6c5428b88958220fef
Tarunika R <127477721+Tarunika-R@users.noreply.github.com>
2025-09-09 12:16:12 +0530

    Time: 0 ms (100.00%) | Memory: 12.8 MB (33.22%) - LeetSync

---
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.py
@@ -0,0 +1,34 @@
+class Solution(object):
+    def search(self, arr, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: bool
+        """
+        n = len(arr)
+        low = 0
+        high = n - 1
+
+        while low <= high:
+            mid = (low + high) // 2
+            
+            if arr[mid] == target:
+                return True
+
+            if arr[low] == arr[mid] and arr[mid] == arr[high]:
+                low += 1
+                high -= 1
+                continue
+
+            if arr[low] <= arr[mid]:
+                if arr[low] <= target <= arr[mid]:
+                    high = mid - 1
+                else:
+                    low = mid + 1
+                
+            else:
+                if arr[mid] <= target <= arr[high]:
+                    low = mid + 1
+                else:
+                    high = mid - 1
+        return False
