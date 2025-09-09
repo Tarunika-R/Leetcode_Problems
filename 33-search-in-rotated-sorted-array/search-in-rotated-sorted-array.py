657bb04e3d7d8519fd5ec82355cbf3b1ee744bc7
Tarunika R <127477721+Tarunika-R@users.noreply.github.com>
2025-09-09 12:04:07 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (43.00%) - LeetSync

---
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def search(self, arr, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        n = len(arr)
+        low = 0
+        high = n - 1
+
+        while low <= high:
+            mid = (low + high) // 2
+
+            if arr[mid] == target:
+                return mid
+            if arr[low] <= arr[mid]:
+                if arr[low] <= target and target <= arr[mid]:
+                    high = mid - 1
+                else: 
+                    low = mid + 1
+            else:
+                if arr[mid] <= target and target <= arr[high]:
+                    low = mid + 1
+                else: 
+                    high = mid - 1
+        return -1
+        
