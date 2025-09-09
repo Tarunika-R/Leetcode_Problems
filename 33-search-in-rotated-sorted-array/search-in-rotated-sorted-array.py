e26c6d3ed4e453172a487bef3ebfab97e1c0489b
Tarunika R <127477721+Tarunika-R@users.noreply.github.com>
2025-09-09 12:14:05 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (5.69%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -15,11 +15,6 @@ class Solution(object):
             if arr[mid] == target:
                 return mid
 
-            if arr[low] == arr[mid] and arr[mid] == arr[high]:
-                low += 1
-                high -= 1
-                continue
-                
             if arr[low] <= arr[mid]:
                 if arr[low] <= target and target <= arr[mid]:
                     high = mid - 1
