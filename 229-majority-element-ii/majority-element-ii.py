756330dd27b30dd8376df85a23b2f34ea5ab5d37
Tarunika R <127477721+Tarunika-R@users.noreply.github.com>
2025-09-10 15:23:22 +0530

    Time: 27 ms (5.69%) | Memory: 15 MB (48.69%) - LeetSync

---
+++ 229-majority-element-ii/majority-element-ii.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        count = Counter(nums)
+        result = []
+        n = len(nums)
+        for i in count:
+            if count[i]>n/3:
+                result.append(i)
+        return result
+    
